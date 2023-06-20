129d1b87451ab83f005736e92173b9db01fd0814
Martin <61461639+xSqne@users.noreply.github.com>
2023-06-20 17:24:01 +0200

    Upload OldestFile.bat

---
+++ OldestFile.bat
@@ -0,0 +1,39 @@
+@echo off
+
+if "%~1"=="" (
+	echo Please provide a path!
+	exit /b
+) else (
+	if "%~1"=="/?" (
+	echo Outputs the oldest file/folder in a path
+	echo Usage: OldestFile.bat [PathtoFolder]
+	exit /b
+	) else (
+		if exist "%~1" (
+			dir %1 /o:d /t:c > temp1.txt
+		) else (
+			echo Path does not exist!
+			exit /b
+		)
+	)
+)
+
+set arg1=%~1
+set driveCheck=%arg1:~-2%
+
+if "%driveCheck%"==":\" (findstr /R /C:".*" /N temp1.txt | find /N "6:" | find "[6]" > temp2.txt) else (
+	findstr /R /C:".*" /N temp1.txt | find /N "8:" | find "[8]" > temp2.txt
+)
+
+for /F "tokens=1,2,3,4,5" %%A in (temp2.txt) do (
+	set initial=%%A
+	set final=%initial:~5%
+	
+	echo The oldest file/folder is %%E, created on %final% at %%B %%C
+	if "%%D"=="<^DIR>" (echo The path is %~1%%E) else (echo The path is %~1\%%E)
+)
+
+del /Q temp1.txt temp2.txt
+
+echo.
+dir %1 /o:d /t:c 
